a94af77356fee6dee892bcda930b9d14552f2760
teurajarvi@hotmail.com <teurajarvi@hotmail.com>
2021-07-02 15:00:49 +0300

    Refactored onSearchSubmit as an arrow function

--- pics-v1/src/components/App.js
+++ pics-v1/src/components/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { pictures: [] };
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,13 +12,14 @@ class App extends React.Component {
       },
     });
 
-    console.log(response.data.results);
-  }
+    this.setState({ pictures: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.pictures.length} images
       </div>
     );
   }
