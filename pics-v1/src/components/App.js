21a353fd6f7d61769739930bd476a5e1c6b12bf9
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-05 11:16:33 +0300

    Added ImageList component

--- pics-v1/src/components/App.js
+++ pics-v1/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { pictures: [] };
@@ -14,9 +15,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
+      <div className='ui container' style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.pictures.length} images
+        <ImageList />
       </div>
     );
   }
