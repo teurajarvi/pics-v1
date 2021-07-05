c8a4a5afa944490c0818aae33c146172c83ff47e
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-05 13:44:11 +0300

    Added map method to list the pictures

--- pics-v1/src/components/App.js
+++ pics-v1/src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
     return (
       <div className='ui container' style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList />
+        <ImageList pictures={this.state.pictures} />
       </div>
     );
   }
